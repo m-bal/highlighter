83a529ae7073fe805950591272cefa6c72ff8275
mbal <mbal2@csub.edu>
2023-04-01 21:49:10 -0700

    Make lua directory during build

--- make.sh
+++ make.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+set -e
 
-DIR="$(dirname "$0")"
+build() {
+  echo "Building silicon.nvim from source..."
 
-if cargo "$@"; then
-    [ -d "$DIR/target/debug" ] && cp "$DIR/target/debug/libhighlighter.dylib" "lua/highlighter.so"
-    [ -d "$DIR/target/release" ] && cp "$DIR/target/release/libhighlighter.dylib" "lua/highlighter.so"
-fi
+  cargo build --release --target-dir ./target
+
+  # Place the compiled library where Neovim can find it.
+  mkdir -p lua
+
+  if [ "$(uname)" == "Darwin" ]; then
+    mv target/release/libhighlighter.dylib lua/highlighter.so
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    mv target/release/libhighlighter.so lua/highlighter.so
+  elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    mv target/release/highlighter.dll lua/highlighter.dll
+  fi
+}
+
+build
